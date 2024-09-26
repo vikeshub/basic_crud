19146db75486d95426aa7aac8497c39c0a37c094
IT Trainee User1 <sspltdev01@SparrowSoftechGroup.com>
2024-09-26 14:50:00 +0530

    updated server code

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import route from "./routes/userRoute.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.MONGOURL;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("db connect successfully");
+
+    app.listen(PORT, () => {
+      console.log(`server is running on port : ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.use("/api", route);
